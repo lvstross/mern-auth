66423ba3960fbee1556fb05b285881abbf9f4dfb
Levi Gonzales <lvstross@gmail.com>
2020-10-10 11:25:56 -0600

    Put all todo routes behind auth middleware and updated post man json file

--- server/src/routes/todo.ts
+++ server/src/routes/todo.ts
@@ -1,12 +1,13 @@
 import express from 'express';
+import { requireSignin } from '../controllers/auth';
 import * as todoRoutes from '../controllers/todo';
 
 // @TODO: Add auth middleware and tie todos to users
 const router = express.Router();
-router.get('/todo', todoRoutes.getAll);
-router.get('/todo/:todoId', todoRoutes.getOne);
-router.post('/todo', todoRoutes.create);
-router.patch('/todo/:todoId', todoRoutes.update);
-router.delete('/todo/:todoId', todoRoutes.deleteOne);
+router.get('/todo', requireSignin, todoRoutes.getAll);
+router.get('/todo/:todoId', requireSignin, todoRoutes.getOne);
+router.post('/todo', requireSignin, todoRoutes.create);
+router.patch('/todo/:todoId', requireSignin, todoRoutes.update);
+router.delete('/todo/:todoId', requireSignin, todoRoutes.deleteOne);
 
 export default router;
